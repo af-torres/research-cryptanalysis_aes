e315acd77cf22a06960e89a6aa003bccec7afd15
Andres <andres.torres.villalba@gmail.com>
2025-11-21 12:55:14 -0500

    fix slurm task queueing bug

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -c 4
-#SBATCH --mem=120G
+#SBATCH --mem=40G
 #SBATCH -G 1
 #SBATCH --nodes=1
 #SBATCH -t 24:00:00
@@ -18,6 +18,6 @@ WORKING_DIR=/home/andres_torres_uri_edu/ondemand/data/sys/myjobs/projects/resear
 
 cd "$WORKING_DIR"
 
-source ./venv/bin/activate
+source .venv/bin/activate
 
-python train.py "${TASK_ARGS[$SLURM_ARRAY_TASK_ID]}"
+python train.py ${TASK_ARGS[$SLURM_ARRAY_TASK_ID]}
