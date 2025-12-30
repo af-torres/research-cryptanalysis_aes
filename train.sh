3ffe7f10d2ba5fa325f3f5d2f5261bbd56a32998
Andres Torres <andres.torres@uri.edu>
2025-12-30 18:16:42 +0000

    update train script to include latest models and increase epochs to 500

--- train.sh
+++ train.sh
@@ -6,6 +6,7 @@
 #SBATCH -t 32:00:00
 #SBATCH --partition=uri-gpu
 
+##### LSTM
 #0 "-d short_128"
 #1 "-d short_192"
 #2 "-d short_256"
@@ -19,14 +20,34 @@
 #10 "-d wiki_rand_iv_192"
 #11 "-d wiki_rand_iv_256"
 
+##### GRU
+#12 "-d wiki_128"
+#13 "-d wiki_192"
+#14 "-d wiki_256"
+#15 "-d wiki_rand_iv_128"
+#16 "-d wiki_rand_iv_192"
+#17 "-d wiki_rand_iv_256"
+
+##### RNN
+#18 "-d wiki_128"
+#19 "-d wiki_192"
+#20 "-d wiki_256"
+#21 "-d wiki_rand_iv_128"
+#22 "-d wiki_rand_iv_192"
+#23 "-d wiki_rand_iv_256"
+
 TASK_ARGS=("-d short_128" "-d short_192" "-d short_256" \
     "-d short_rand_iv_128" "-d short_rand_iv_192" "-d short_rand_iv_256" \
-    "-d wiki_128" "-d wiki_192" "-d wiki_256" \
-    "-d wiki_rand_iv_128" "-d wiki_rand_iv_192" "-d wiki_rand_iv_256")
+    "-d wiki_128 -m lstm" "-d wiki_192 -m lstm" "-d wiki_256 -m lstm" \
+    "-d wiki_rand_iv_128 -m lstm" "-d wiki_rand_iv_192 -m lstm" "-d wiki_rand_iv_256 -m lstm" \
+    "-d wiki_128 -m gru" "-d wiki_192 -m gru" "-d wiki_256 -m gru" \
+    "-d wiki_rand_iv_128 -m gru" "-d wiki_rand_iv_192 -m gru" "-d wiki_rand_iv_256 -m gru" \
+    "-d wiki_128 -m rnn" "-d wiki_192 -m rnn" "-d wiki_256 -m rnn" \
+    "-d wiki_rand_iv_128 -m rnn" "-d wiki_rand_iv_192 -m rnn" "-d wiki_rand_iv_256 -m rnn")
 WORKING_DIR=/work/pi_kelum_gajamannage_uri_edu/research-cryptanalysis_aes
 
 cd "$WORKING_DIR"
 
 source ./start_venv.sh
 
-python train.py ${TASK_ARGS[$SLURM_ARRAY_TASK_ID]} -e 100 -bs 500 -ms 30000
+python train.py ${TASK_ARGS[$SLURM_ARRAY_TASK_ID]} -e 500 -bs 500 -ms 30000
